284f7c102743c2c3afa219dccbbfb27a7deb665f
psnwd <52720489+psnwd@users.noreply.github.com>
2020-07-29 09:50:13 +0530

    Update Job.sql

--- SQL Query/Job.sql
+++ SQL Query/Job.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE Job(
+	Job_ID int PRIMARY KEY NOT NULL IDENTITY(1000,1),
+	Start_Location varchar(50),
+	End_Location varchar(50),
+	Job_Date datetime,
+	Load_ID int FOREIGN KEY REFERENCES Load(Load_ID),
+	Customer_ID int FOREIGN KEY REFERENCES Customer(Customer_ID)
+);
