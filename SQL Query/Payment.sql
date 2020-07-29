57f7ee6dcc1e6a44f218959d9debb830d0dc18bc
psnwd <52720489+psnwd@users.noreply.github.com>
2020-07-29 09:50:31 +0530

    Update Payment.sql

--- SQL Query/Payment.sql
+++ SQL Query/Payment.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE Payment(
+	Pay_ID int PRIMARY KEY NOT NULL IDENTITY(1000,1),
+	Pay_Type varchar(50),
+	Pay_Date datetime,
+	Amount money,
+	Job_ID int FOREIGN KEY REFERENCES Job(Job_ID),
+	Customer_ID int FOREIGN KEY REFERENCES Customer(Customer_ID)
+);
