7168d009c94fcaa5398dcb12e2a3622de8e453f2
psnwd <52720489+psnwd@users.noreply.github.com>
2020-07-29 09:49:47 +0530

    Update Load.sql

--- SQL Query/Load.sql
+++ SQL Query/Load.sql
@@ -1,1 +1,6 @@
-
+CREATE TABLE Load(
+	Load_ID int PRIMARY KEY NOT NULL IDENTITY(1000,1),
+	Load_type varchar(50),
+	Load_Date datetime,
+	Product_ID int FOREIGN KEY REFERENCES Product(Product_ID)
+);
