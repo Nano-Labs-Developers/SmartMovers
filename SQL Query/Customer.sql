6703eed12dd4c9a022688dc7b346ae74907f7e25
psnwd <52720489+psnwd@users.noreply.github.com>
2020-07-29 09:49:06 +0530

    Update Customer.sql

--- SQL Query/Customer.sql
+++ SQL Query/Customer.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE Customer(
+	Customer_ID int PRIMARY KEY NOT NULL IDENTITY(1000,1),
+	Customer_Name varchar(100),
+	Customer_Address varchar(100),
+	Customer_ContactNo int,
+	Customer_type varchar(50),
+);
